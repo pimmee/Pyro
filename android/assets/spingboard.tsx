fcaccd7a46fabb6bb7538347e5f305c7226e4912
simsu451 <simsu451@student.liu.se>
2017-11-14 23:29:48 +0100

    assets

---
+++ android/assets/spingboard.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="springboardUp" tilewidth="70" tileheight="70" tilecount="1" columns="1">
+ <image source="springboardUp.png" trans="000000" width="70" height="70"/>
+</tileset>
